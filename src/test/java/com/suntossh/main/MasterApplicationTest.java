e6ec72e33c879ba7b5b9fadf466fb8bd58d3ae04
Suntossh <suntossh.numerouno@gmail.com>
2018-11-17 23:27:49 -0500

    master 1 to 2

--- src/test/java/com/suntossh/main/MasterApplicationTest.java
+++ src/test/java/com/suntossh/main/MasterApplicationTest.java
@@ -4,6 +4,6 @@ package com.suntossh.main;
 public class MasterApplicationTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Developer Main Test 1");
+		System.out.println("Hello Developer Main Test 12");
 	}
 }
